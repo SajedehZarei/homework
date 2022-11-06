f7568b9e6250f1dba14b01bbfeaf74e15971ef53
sajdexr <sajedehzarei1381@gmail.com>
2022-11-06 23:59:37 +0330

    add home2

---
+++ home2.cs
@@ -0,0 +1,16 @@
+using system;
+using system.Collections.Generic;
+using system.Text.RegularExpressions;
+using system.linq;
+namespace Rextester
+{
+    public class Program
+    {
+     public static void Main (string[] args)   
+     {
+        string word;
+        word=Console.ReadLine();
+        Console.WriteLine("say {0}",word);
+     }
+    }
+}
