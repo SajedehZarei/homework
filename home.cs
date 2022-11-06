79efddc7edf6f0e506ad90b174cd30dccf6b87a6
sajdexr <sajedehzarei1381@gmail.com>
2022-11-06 23:20:25 +0330

    commit

---
+++ home.cs
@@ -0,0 +1,17 @@
+using system;
+using system.Collections.Generic;
+using system.Text.RegularExpressions;
+using system.linq;
+namespace Rextester
+{
+    public class Program
+    {
+     public static void Main (string[] args)   
+     {
+        bool content = true;
+        bool nocontent = false;
+        Console.writeLine("test is {0}" , content);
+        Console.writeLine("test is {0}" ,nocontent);
+     }
+    }
+}
